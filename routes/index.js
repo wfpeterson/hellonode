8cc4789b4825e36a617d0a5a9598c7c968f644be
wfpeterson <John316>
2016-02-20 22:29:54 -0500

    Updates while testing scenarios.

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,34 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/check', function(req, res) {
+  var responseObj = { message: 'Up and Running' };
+  res.send(responseObj);
+});
+
+var database = [];
+
+router.post('/ilike/:icecream/:name', function(req, res){
+  if(req.body.StudyList){
+    var obj = req.body.StudyList;
+  //}
+  //if(req.body.formfactor){
+    console.log(obj);
+  }
+  else{
+    console.log('No StudyList');
+  }
+  var choice = req.params.icecream;
+  var nm = req.params.name;
+
+  database.push({choice: choice, name: nm});
+  var responseObj = {message: nm + ' likes ' + choice + '.'};
+  res.send(responseObj)
+
+});
+
+router.get('/likes', function(req,res){
+  res.send(database);
+});
+
 module.exports = router;
